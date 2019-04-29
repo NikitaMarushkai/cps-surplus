653119b46ef40e994a0d15957047a432a744dd00
nikita.marushkai <nikita.marushkai@setinbox.com>
2019-04-29 17:01:23 +0300

    First commit

---
+++ src/main/groovy/com/cpssurplus/controllers/IndexController.groovy
@@ -0,0 +1,13 @@
+package com.cpssurplus.controllers
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class IndexController {
+
+    @GetMapping("/")
+    String index() {
+        return "index"
+    }
+}
